83d4ca14539787b245b27b194a7bbc02bb79fa63
altimateshaikh <94660335+altimateshaikh@users.noreply.github.com>
2024-09-26 14:45:53 -0700

    sa

---
+++ project-1/main.js
@@ -0,0 +1,9 @@
+// document.addEventListener("DOMContentLoaded",function(){})
+    document.querySelector(".run").addEventListener("mouseover", function(){
+        console.log("change")
+        document.querySelector(".run").innerHTML = "you realy dont have to"
+    })
+
+//store text in an array
+//every time mouseover move to next item into array
+//look
